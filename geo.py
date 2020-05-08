4808a399600e2bd82c539439d81f88afb39b09aa
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2020-05-08 09:07:35 +0400

    F

--- geo.py
+++ geo.py
@@ -7,7 +7,8 @@ def get_coordinates(city):
 
     params = {
         'geocode': city,
-        'format': 'json'
+        'format': 'json',
+        'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
     }
 
     response = requests.get(url, params)
@@ -24,7 +25,8 @@ def get_country(city):
 
     params = {
         'geocode': city,
-        'format': 'json'
+        'format': 'json',
+        'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
     }
 
     response = requests.get(url, params)
